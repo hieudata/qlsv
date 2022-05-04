7361a578f9576a64944743e526ec5b326afcf665
hieudata <hz.hieu.kun.st@gmail.com>
2022-05-04 14:36:53 +0700

    first commit

---
+++ database/migrations/2022_04_27_024918_create_students_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStudentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name',55);
+            $table->string('avatar',55);
+            $table->string('phone',15);
+            $table->tinyInteger('gender');
+            $table->string('email',55);
+            $table->date('birthday');
+            $table->integer('faculty_id')->unsigned();
+            $table->foreign('faculty_id')->references('id')->on('faculties');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('students');
+    }
+}
---
+++ database/seeders/FacultySeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Faculty;
+use Illuminate\Database\Seeder;
+
+class FacultySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Faculty::factory()->times(50)->create();
+    }
+}
